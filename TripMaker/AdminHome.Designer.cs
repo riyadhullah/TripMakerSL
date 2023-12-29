e045d6d42cf882ad30b95f571fe1f3e3adc28fe9
riyadhullah <md.riyadhullah1@gmail.com>
2023-12-30 03:59:54 +0600

    manage Hotel Rooms

---
+++ TripMaker/AdminHome.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace TripMaker
+{
+    partial class AdminHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(381, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Manage Hotel Rooms";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(663, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AdminHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 800);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "AdminHome";
+            this.Text = "Home page";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
--- TripMaker/DataAccess.cs
+++ TripMaker/DataAccess.cs
@@ -10,7 +10,7 @@ namespace TripMaker
 {
     static class DataAccess
     {
-        public static string conection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\AIUB\Semester 05\C#Project\TripMakerSL\TripMakerDB.mdf;Integrated Security=True;Connect Timeout=30";
+        public static string conection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\C# project\TripMakerSL\TripMakerDB.mdf;Integrated Security=True;Connect Timeout=30";
      
 
         public static void ExecuteData(string query, out string error)
