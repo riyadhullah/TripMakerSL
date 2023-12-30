edfc6168b5f62bec74b8cf83577f5c36a0d6fec1
MinhajunNabi <153017394+MinhajunNabi@users.noreply.github.com>
2023-12-31 05:50:06 +0600

    user control

---
+++ TripMaker/UserControl1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace TripMaker
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDhakatoCTG = new System.Windows.Forms.Panel();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
+            this.panelDhakatoCTG.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelDhakatoCTG
+            // 
+            this.panelDhakatoCTG.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelDhakatoCTG.Controls.Add(this.label21);
+            this.panelDhakatoCTG.Controls.Add(this.label22);
+            this.panelDhakatoCTG.Controls.Add(this.label23);
+            this.panelDhakatoCTG.Controls.Add(this.label24);
+            this.panelDhakatoCTG.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelDhakatoCTG.Location = new System.Drawing.Point(119, 15);
+            this.panelDhakatoCTG.Name = "panelDhakatoCTG";
+            this.panelDhakatoCTG.Size = new System.Drawing.Size(629, 120);
+            this.panelDhakatoCTG.TabIndex = 32;
+            this.panelDhakatoCTG.Visible = false;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.Location = new System.Drawing.Point(18, 82);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(152, 23);
+            this.label21.TabIndex = 4;
+            this.label21.Text = "Take Off: 3 pm";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.ForeColor = System.Drawing.Color.Red;
+            this.label22.Location = new System.Drawing.Point(496, 77);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(94, 23);
+            this.label22.TabIndex = 2;
+            this.label22.Text = "4000 BDT";
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label23.Location = new System.Drawing.Point(152, 47);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(312, 21);
+            this.label23.TabIndex = 1;
+            this.label23.Text = "Dhaka Airport to Chittagong Airport";
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label24.Location = new System.Drawing.Point(18, 15);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(192, 23);
+            this.label24.TabIndex = 0;
+            this.label24.Text = "Bangladesh Biman";
+            this.label24.Visible = false;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelDhakatoCTG);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(866, 150);
+            this.panelDhakatoCTG.ResumeLayout(false);
+            this.panelDhakatoCTG.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDhakatoCTG;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label24;
+    }
+}
